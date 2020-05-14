d198dc7fd10307a7f3d422b56be87d4cdfb275c4
VuVanHoc <hocvanvu1999@gmail.com>
2020-05-14 10:13:20 +0700

    init project BE

---
+++ Web3306-master/license_test/src/main/java/com/uet/int3115/nhom8/license_test/LicenseTestApplication.java
@@ -0,0 +1,13 @@
+package com.uet.int3115.nhom8.license_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseTestApplication.class, args);
+	}
+
+}
---
+++ Web3306-master/license_test/src/test/java/com/uet/int3115/nhom8/license_test/LicenseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uet.int3115.nhom8.license_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
